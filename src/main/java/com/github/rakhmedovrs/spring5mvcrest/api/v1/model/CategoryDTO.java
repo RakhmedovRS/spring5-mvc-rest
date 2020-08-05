3a4653d8106eda58ef81f9d6f00e1c0de64e0b5a
rakhmedovrs <rakhmedovrs@gmail.com>
2020-08-05 21:39:44 +0300

    Added category classes. closes #1

---
+++ src/main/java/com/github/rakhmedovrs/spring5mvcrest/api/v1/model/CategoryDTO.java
@@ -0,0 +1,11 @@
+package com.github.rakhmedovrs.spring5mvcrest.api.v1.model;
+
+/**
+ * @author RakhmedovRS
+ * @created 05-Aug-20
+ */
+public class CategoryDTO
+{
+	private Long id;
+	private String name;
+}
---
+++ src/main/java/com/github/rakhmedovrs/spring5mvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.github.rakhmedovrs.spring5mvcrest.repositories;
+
+import com.github.rakhmedovrs.spring5mvcrest.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author RakhmedovRS
+ * @created 05-Aug-20
+ */
+public interface CategoryRepository extends JpaRepository<Category, Long>
+{
+
+}
