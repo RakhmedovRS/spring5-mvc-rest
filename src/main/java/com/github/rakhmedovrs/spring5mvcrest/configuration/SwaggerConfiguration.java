fceebd57e3602f7ae081b0f4d2acdf31242e3c2e
rakhmedovrs <rakhmedovrs@gmail.com>
2020-09-02 22:22:47 +0300

    Customized Swagger with Meta Data. closes #19

--- src/main/java/com/github/rakhmedovrs/spring5mvcrest/configuration/SwaggerConfiguration.java
+++ src/main/java/com/github/rakhmedovrs/spring5mvcrest/configuration/SwaggerConfiguration.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 /**
  * @author RakhmedovRS
@@ -23,6 +27,23 @@ public class SwaggerConfiguration
 			.select().apis(RequestHandlerSelectors.any())
 			.paths(PathSelectors.any())
 			.build()
-			.pathMapping("/");
+			.pathMapping("/")
+			.apiInfo(metaData());
+	}
+
+	private ApiInfo metaData()
+	{
+		Contact contact = new Contact("Ruslan Rakhmedov", "https://github.com/RakhmedovRS",
+			"rakhmedovrs@gmmail.com");
+
+		return new ApiInfo(
+			"Rest API examples",
+			"",
+			"1.0",
+			"Terms of Service: blah",
+			contact,
+			"Apache License Version 2.0",
+			"https://www.apache.org/licenses/LICENSE-2.0",
+			Collections.emptyList());
 	}
 }
